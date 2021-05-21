a2b2ef4ba8fa1b5ba3cb838928d00d1300ef98d6
Abhishek Sharma <abhisheksharma@geotab.com>
2021-05-20 21:34:32 -0400

    Did some BFS practice.

--- Problems/BreathFirstSearch.cs
+++ Problems/BreathFirstSearch.cs
@@ -11,24 +11,24 @@ namespace TestProject.Problems
             BNode DataNode = root;
             List<int> result = new List<int>();
 
-            if(root!= null)
+            if (root != null)
             {
                 tempQueue.Enqueue(root);
 
-                while(tempQueue.Count > 0)
+                while (tempQueue.Count > 0)
                 {
                     DataNode = tempQueue.Dequeue();
 
                     result.Add(DataNode.value);
 
-                    if(DataNode.left!=null)
+                    if (DataNode.left != null)
                     {
                         tempQueue.Enqueue(DataNode.left);
                     }
-                    if(DataNode.right!=null)
+                    if (DataNode.right != null)
                     {
                         tempQueue.Enqueue(DataNode.right);
-                    }           
+                    }
                 }
             }
             else
@@ -39,5 +39,68 @@ namespace TestProject.Problems
             return result;
         }
 
+        public static List<List<int>> BFSTest(BNode root)
+        {
+            List<List<int>> output = new List<List<int>>();
+
+            List<int> result = new List<int>();
+
+            if(root == null)
+            {
+                return output;
+            }
+
+            BNode temp = new BNode();
+
+            Queue<BNode> elements = new Queue<BNode>();
+            elements.Enqueue(root);
+            int count = 0;
+            bool isRightSidePrint = true;
+
+            while(elements.Count>0)
+            {
+                count = elements.Count;
+
+                while(count > 0)
+                {
+                    temp = elements.Dequeue();
+
+                    result.Add(temp.value);
+
+                    if (!isRightSidePrint)
+                    {
+
+                        if (temp.left != null)
+                        {
+                            elements.Enqueue(temp.left);
+                        }
+
+                        if (temp.right != null)
+                        {
+                            elements.Enqueue(temp.right);
+                        }
+                    }
+                    else
+                    {
+                        if (temp.right != null)
+                        {
+                            elements.Enqueue(temp.right);
+                        }
+                        if (temp.left != null)
+                        {
+                            elements.Enqueue(temp.left);
+                        }
+
+                    }
+                    count--;
+                }
+
+                isRightSidePrint = !isRightSidePrint;
+                output.Add(result);
+                result = new List<int>();
+            }
+
+            return output;
+        }
     }
 }
